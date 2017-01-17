2aa8c7aa47f740b5d3d5fa239ee6018eafe93e0d
Ben Rotter <BenJRotter@gmail.com>
2017-01-16 15:04:41 -1000

    added cuts and pulser separation

--- globClusterFiles.C
+++ globClusterFiles.C
@@ -20,6 +20,11 @@ void globClusterFiles(){
 
   TH2D *hInterVsHilb = new TH2D("hInterVsHilb","Interferometric Peak vs Hilbert Peak;Interferometric Peak (?); Hilbert Peak (mV)",   150,0,0.15,  100,0,100);
 
+  TH2D *hWaisPulses = new TH2D("hWaisPulses","Interferometric Peak vs Hilbert Peak (WAIS Pulses);Interferometric Peak (?); Hilbert Peak (mV)",   150,0,0.15,  100,0,100);
+
+
+  TH2D *hLDBPulses = new TH2D("hLDBPulses","Interferometric Peak vs Hilbert Peak (WAIS Pulses);Interferometric Peak (?); Hilbert Peak (mV)",   150,0,0.15,  100,0,100);
+
   
 
   //Lets make the summary object that I can shove into the output tree
@@ -30,6 +35,8 @@ void globClusterFiles(){
   int lenEntries = resultTree->GetEntries();
   cout << "Found " << lenEntries << " Entries" << endl;
 
+  int skippedEvents = 0;
+
   for (int entry=0; entry< lenEntries; entry++) {
     if (entry%10000 == 0) {
       cout << entry << "/" << lenEntries << "\r";
@@ -37,8 +44,30 @@ void globClusterFiles(){
     }
 
     resultTree->GetEntry(entry);
-    hInterVsHilb->Fill(eventSummary->peak[0][0].value,eventSummary->coherent[0][0].peakHilbert);
+
+    //cuts
+    if ( (eventSummary->flags.isPayloadBlast == 1) ||
+	 (eventSummary->flags.isGood == 0) ||
+	 (eventSummary->peak[0][0].masked == 1) ) {
+      skippedEvents++;
+      continue;
+    }
+	
+    
+    //split into pulser and non-pulser
+    if (eventSummary->flags.pulser == 1) {
+      hWaisPulses->Fill(eventSummary->peak[0][0].value,eventSummary->coherent[0][0].peakHilbert);
+    }
+    else if (eventSummary->flags.pulser == 2) {
+      hLDB->Fill(eventSummary->peak[0][0].value,eventSummary->coherent[0][0].peakHilbert);
+    }
+    else {
+      hInterVsHilb->Fill(eventSummary->peak[0][0].value,eventSummary->coherent[0][0].peakHilbert);
+    }
+
   }
+
+  cout << "Skipped " << skippedEvents << " events from cuts" << endl;
 
   hInterVsHilb->Draw("colz");
  
