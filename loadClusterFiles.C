938a083a5ee74f6b6c11fa9f15fced2092f195f1
Ben Rotter <BenJRotter@gmail.com>
2017-01-20 13:20:58 -1000

    added dir to loadClusterFiles

--- loadClusterFiles.C
+++ loadClusterFiles.C
@@ -1,8 +1,11 @@
 #include "AnitaEventSummary.h"
 #include <sys/stat.h>
 
-void loadClusterFiles(){
+void loadClusterFiles(string dataDir="0"){
 
+  if (dataDir == "0") {
+    dataDir = "firstAttempt";
+  }
 
   TChain *resultTree = new TChain("headTree","headTree");
 
@@ -11,7 +14,7 @@ void loadClusterFiles(){
   struct stat buffer;   
   for (int run=130; run<384; run++) {
     name.str("");
-    name << "/home/brotter/nfsShared/results/findCosmicRays/" << run << ".root";
+    name << "/home/brotter/nfsShared/results/findCosmicRays/" << dataDir << "/" << run << ".root";
 
     if (stat (name.str().c_str(), &buffer) == 0) {
       resultTree->Add(name.str().c_str()); }
