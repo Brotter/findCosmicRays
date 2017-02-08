2b3e8686f346c0a070c8baaf7ceffaf4df3a581f
Ben Rotter <BenJRotter@gmail.com>
2017-02-08 11:32:37 -1000

    clusterClusterer.cc now looks to the resultFile for gps stuff

--- clusterClusterer.cc
+++ clusterClusterer.cc
@@ -98,18 +98,18 @@ void clusterClusterer(string outFileName, int numCores=1, int core=0){
   //UsefulAdu5Pat has a nice thing for taking a lat/lon and turning it back into an angle
   //this should be included in the fisherCut resultTree now... but just check to make sure
   TChain *gpsTree = new TChain("adu5PatTree","adu5PatTree");
-  //  gpsTree->Add(fisherCutName.c_str());
+  gpsTree->Add(fisherCutName.c_str());
   
 
   char* dataDir = getenv("ANITA3_DATA");
   stringstream name;
   //  if (gpsTree->GetEntries() == 0) {
-  cout << "didn't find the gps stuff in the resultTree so I'm loading it all" << endl;
-  for (int run=130; run<440; run++) {
-    name.str("");
-    name << dataDir << "run" << run << "/gpsEvent" << run << ".root";
-      gpsTree->Add(name.str().c_str());
-  }
+//  cout << "didn't find the gps stuff in the resultTree so I'm loading it all" << endl;
+//  for (int run=130; run<440; run++) {
+//    name.str("");
+//    name << dataDir << "run" << run << "/gpsEvent" << run << ".root";
+//      gpsTree->Add(name.str().c_str());
+//  }
     //  }
 
   Adu5Pat *pat = NULL;
