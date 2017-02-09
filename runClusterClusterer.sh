094f4e3cd09ab48e11cae41801e74dd68cf379d5
Ben Rotter <BenJRotter@gmail.com>
2017-02-08 23:40:08 -1000

    okay also running over 0.5M events just takes awhile dduh.  Lets involve all the servers in this fun game.

--- runClusterClusterer.sh
+++ runClusterClusterer.sh
@@ -14,7 +14,24 @@ sharedDir="/home/brotter/nfsShared/results/clusterClusterer/"${startTime}
 mkdir ${sharedDir}
 mkdir ${sharedDir}"/log"
 
-for core in `seq 0 63`; do
-    nice ./clusterClusterer ${sharedDir}/${core}".root" 64 $core 1> ${sharedDir}/log/${core}.log 2>&1 &
+if [ `hostname | cut -d"." -f1` == "anitaI" ]; then
+    host=0
+elif [ `hostname | cut -d"." -f1` == "anitaII" ]; then
+    host=1
+elif [ `hostname | cut -d"." -f1` == "anitaIII" ]; then
+    host=2
+elif [ `hostname | cut -d"." -f1` == "anitaIV" ]; then
+    host=3
+else
+    echo "The server isn't an anita cluster server, so you shouldn't use this script"
+    exit
+fi
+
+
+startSeq=$((host*64))
+stopSeq=$(((host+1)*64-1))
+
+for core in `seq ${startSeq} ${stopSeq}`; do
+    nice ./clusterClusterer ${sharedDir}/${core}".root" 256 $core 1> ${sharedDir}/log/${core}.log 2>&1 &
 done
     
